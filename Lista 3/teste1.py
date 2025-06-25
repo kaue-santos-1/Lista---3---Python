70534617f035d559c94b4287b0322ed0e4fcc057
kaue-santos-1 <kaue.henrique@outlook.com>
2025-06-25 14:26:26 -0300

    Add files via upload

---
+++ Lista 3/teste1.py
@@ -0,0 +1,22 @@
+lista_de_numeros = [1,2,3,4,5,6,7,8,9,10]
+lista_de_nomes = ['Kaue', 'Kethlyn', 'Laura', 'Cacá']
+lista_de_ano = [1997,2025]
+
+lista_de_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for numero in lista_de_numeros:
+        print(numero)
+
+soma_impares = 0
+for i in range(1, 11, 2):
+    soma_impares += i
+print(soma_impares)
+
+soma_pares = 0
+for i in range(2, 11, 2):
+    soma_pares += i
+print(soma_pares)
+
+for i in range(10, 0, -1):
+    print(i)
+
+
---
+++ Lista 3/teste3.py
@@ -0,0 +1,9 @@
+lista_numeros = [10, 2, 8, 17, 7]
+soma = 0
+
+try:
+    for numero in lista_numeros:
+        soma += numero
+    print(f"Soma dos elementos: {soma}")
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
